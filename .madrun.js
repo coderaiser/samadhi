5d7658719349d0deff8b1c9004be892251e574e1
coderiaser <coderaiser@cloudcmd.io>
2025-03-26 23:37:27 +0200

    chore: lint

--- .madrun.js
+++ .madrun.js
@@ -9,6 +9,6 @@ module.exports = {
     'fix:lint': () => run('lint', '--fix'),
     'report': () => 'c8 report --reporter=lcov',
     'watcher': () => 'nodemon -w test -w lib --exec',
-    'watch:test': async () => await run('watcher', await run('test')),
+    'watch:test': async () => await run('watcher', `"${await run('test')}"`),
     'watch:lint': async () => await run('watcher', await run('lint')),
 };
